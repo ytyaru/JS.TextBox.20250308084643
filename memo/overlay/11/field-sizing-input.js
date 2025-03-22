eaf35e6058f173fae952638f2c86a999eb78a0b8
ytyaru <yttry0@gmail.com>
2025-03-22 10:54:19 +0900

    1

---
+++ memo/overlay/11/field-sizing-input.js
@@ -0,0 +1,125 @@
+class FieldSizingInput extends HTMLElement {
+    constructor() {
+        super();
+        this._shadow = this.attachShadow({mode:'open'});
+        this.#makeStyle();
+        this.#makeEl();
+        // フォーカスを失ったらキャレットを先頭に戻す
+        this.#inputEl.addEventListener('blur', e=>{
+            e.target.selectionStart = 0;
+            e.target.selectionEnd = 0;
+        })
+    }
+    #makeStyle(shadow) {
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(`
+:host {
+    position: relative;
+    display: inline-block;
+    background: red;
+    max-width: 100%;
+}
+:host input {
+    box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+}
+:host .dummy {
+    background-color:red;
+    font-size: 16px;
+    display: inline-block;
+    overflow: hidden;
+    white-space: nowrap;
+    opacity: 0;
+    box-sizing: border-box;
+    max-width: 100%;
+}
+:host .dummy::before {content: '';}
+:host .dummy:empty::before {content: attr(data-placeholder);}
+:host .font {
+    font-family: monoscape;
+    font-size: 16px;
+    box-sizing: border-box;
+    padding:0; margin:0;
+    line-height: 1.5em;
+    letter-spacing: 0.05em;
+    min-width: 1em;
+}
+`);
+        this._shadow.adoptedStyleSheets = [sheet];
+    }
+    #makeEl(shadow) {
+        const dummy = document.createElement('div');
+        const input = document.createElement('input');
+        dummy.classList.add('dummy', 'font');
+        input.classList.add('input', 'font');
+        dummy.dataset.placeholder = 'プレースホルダー';
+        input.placeholder = 'プレースホルダー';
+        input.addEventListener('input', (e)=>{
+            this.#dummyEl.textContent = e.target.value;
+            this.resize();
+        });
+        this._shadow.append(dummy, input);
+    }
+    resize() {
+        const parentWidth = parseInt(getComputedStyle(this.parentElement.parentElement).width);
+        const width = this.#dummyEl.clientWidth + 4;
+        this.#inputEl.style.width = `${Math.min(width, parentWidth)}px`;
+        console.log(parentWidth, width, this.#inputEl.style.width)
+    }
+    connectedCallback() {
+//        console.log("カスタム要素がページに追加されました。");
+        console.log(`${this.#dummyEl.clientWidth + 4}px`)
+        this.resize()
+    }
+//    connectedCallback() {console.log("カスタム要素がページに追加されました。");}
+//    disconnectedCallback() {console.log("カスタム要素がページから除去されました。");}
+//    adoptedCallback() {console.log("カスタム要素が新しいページへ移動されました。");}
+    attributeChangedCallback(name, oldValue, newValue) {
+//        console.log(`属性 ${name} が変更されました。`);
+             if ('value'===name) {this.value=newValue}
+        else if ('placeholder'===name) {this.placeholder=newValue}
+        else if ('font-size'===name) {this.fontSize=newValue}
+        else if ('font-family'===name) {this.fontFamily=newValue}
+        else if ('max-width'===name) {this.maxWidth=newValue}
+    }
+    static get observedAttributes() {return ['value', 'placeholder', 'font-family', 'font-size', 'max-width'];}
+    get #inputEl() {return this._shadow.querySelector('input')}
+    get #dummyEl() {return this._shadow.querySelector('.dummy')}
+    get value() {return this.#inputEl.value}
+    set value(v) {
+        this.#inputEl.value = v;
+        this.#dummyEl.textContent = v.replace(/\r\n|\r|\n/gm, '');
+        this.resize();
+    }
+    get placeholder(){return this.#inputEl.placeholder}
+    set placeholder(v) {
+        this.#inputEl.placeholder = v;
+        this.#dummyEl.dataset.placeholder = v;
+    }
+    get fontFamily(){return getComputedStyle(this.#inputEl).fontFamily}
+    set fontFamily(v) {
+        this.#inputEl.style.fontFamily = v;
+        this.#dummyEl.style.fontFamily = v;
+        console.log(this.fontFamily);
+    }
+    get fontSize(){return getComputedStyle(this.#inputEl).fontSize}
+    set fontSize(v) {
+        this.#inputEl.style.fontSize= v;
+        this.#dummyEl.style.fontSize= v;
+        console.log(this.fontSize);
+    }
+    get maxWidth(){return getComputedStyle(this.#inputEl).maxWidth}
+    set maxWidth(v) {
+        this.style.maxWidth = v;
+//        this.#inputEl.style.maxWidth = v;
+//        this.#dummyEl.style.maxWidth = v;
+        console.log(this.maxWidth);
+    }
+}
+customElements.define('field-sizing-input', FieldSizingInput);
---
+++ memo/overlay/11/overlay-box.js
@@ -0,0 +1,132 @@
+;(function(){
+class OverlayBoxElement extends HTMLElement {
+    constructor(options) {// baseEl:el, ...
+        super();
+        this._options = {...this.defaultOptions, ...options};
+//        this.#makeDefaultBaseEl();
+        this._shadow = this.attachShadow({mode:'open'});
+    }
+    connectedCallback() {
+        console.log("カスタム要素がページに追加されました。");
+        // this.textContent を取得するにはsetTimeoutせねばならない! クソ仕様
+        // https://stackoverflow.com/questions/64169068/obtain-this-textcontent-during-custom-element-construction
+        console.log(this.textContent)
+        setTimeout(()=>{
+            console.log('setTimeout:', this.textContent);
+            console.log('setTimeout:', this.innerHTML);
+            this.#makeDefaultBaseEl();
+            this.#makeEl();
+            this.#addEvent();
+            console.log('setTimeout:', this.children);
+        }, 0);
+    }
+    disconnectedCallback() {console.log("カスタム要素がページから除去されました。");}
+    adoptedCallback() {console.log("カスタム要素が新しいページへ移動されました。");}
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(`属性 ${name} が変更されました。`);
+    }
+    static get observedAttributes() {return [''];}
+
+    get el() {return this._shadow.querySelector(`div[name="overlay-box"]`)}
+    get baseEl() {return this._shadow.querySelector(`.base-el`)}
+    get defaultOptions() { return {
+        baseEl: null,
+    } }
+    #makeStyle() {
+        const style = document.createElement('style');
+        style.textContent = `
+div[name="overlay-box"] {
+    position: sticky; /*relative, absolute, fixed, sticky*/
+    margin:0; padding:0;
+    box-sizing: border-box;
+    width:300px; height:50vh;
+}
+div[name="overlay-box"] .base-el {
+    position: absolute; top:0; left:0;
+    box-sizing: border-box;
+    width:100%; height:100%;
+}
+div[name="overlay-area"] {
+    display: block;
+    position: absolute; top:0; left:0;
+    background-color: red;
+    opacity: 0.5;
+    width:100%; height:100%;
+    pointer-events: none; /*マウスイベント透過*/
+}
+div[name="overlay-area"] .child {
+    position: absolute;
+    box-sizing: border-box;
+    pointer-events: auto;
+}
+div[name="top-left"] {top:0; left:0;}
+div[name="top"] {top:0; left:50%; transform:translate(-50%,0); max-width:fit-content;}
+div[name="top-right"] {top:0; right:0;}
+div[name="left"] {top:50%; left:0; transform:translate(0,-50%); max-width:fit-content;}
+div[name="center"] {top:50%; left:50%; transform:translate(-50%,-50%); max-width:fit-content;}
+div[name="right"] {top:50%; right:0; transform:translate(0,-50%); max-width:fit-content;}
+div[name="bottom-left"] {bottom:0; left:0;}
+div[name="bottom"] {bottom:0; left:50%; transform:translate(-50%,0); max-width:100%;}
+div[name="bottom-right"] {bottom:0; right:0;}
+`;
+        return style;
+    }
+    #makeDefaultBaseEl() {
+        const innerEl = [...this.children].find(el=>'base'===el.getAttribute('name'));
+        if (innerEl) {this._options.baseEl = innerEl}
+        if (this._options.baseEl instanceof HTMLElement) {
+            if (!this._options.baseEl.classList.contains('base-el')) {
+                this._options.baseEl.classList.add('base-el');
+            }
+        } else {
+            const el = document.createElement('textarea');
+            el.classList.add('base-el');
+            this._options.baseEl = el;
+        }
+    }
+    #makeEl() {
+        const root = document.createElement('div');
+        root.setAttribute('name', 'overlay-box');
+        root.classList.add('overlay-box');
+        root.append(
+            this.#makeStyle(),
+            this._options.baseEl,
+            this.#makeArea(),
+        );
+        this._shadow.append(root);
+    }
+    #makeArea() {
+        const area = document.createElement('div');
+        area.setAttribute('name', 'overlay-area');
+        area.classList.add('overlay-area');
+        //area.append(this.#makeStyle(), 'overlay-area')
+        const NAMES = {
+            type1: 'top-left top top-right left center right bottom-left bottom bottom-right'.split(' '),
+            type2: 'north-west north north-east west center east south-west south south-east'.split(' '),
+        }
+        // 対象要素のみ取得する。余計な要素は消す。
+        for (let node of this.childNodes) {
+            if (Node.ELEMENT_NODE===node.nodeType) {
+                if ([1,2].map(v=>NAMES[`type${v}`].includes(node.getAttribute('name'))).some(v=>v)) {
+                    node.classList.add('child');
+                    area.append(node)
+                }
+                else {node.remove()}
+            }
+        }
+        return area;
+    }
+    #addEvent() {
+        (new ResizeObserverX((entries,observer)=>{
+            const E = entries[0].target.style;
+            const B = entries[0].border[0];
+            const T = this.el.style;
+            E.contentVisibility = 'hidden';
+            T.width = B.width;
+            T.height = B.height;
+            E.contentVisibility = 'visible';
+        })).observe(this.baseEl);
+    }
+}
+customElements.define('overlay-box', OverlayBoxElement);
+})();
